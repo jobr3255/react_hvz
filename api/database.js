e3036a102410996d9770b55c13e7ae173879b49f
Josh Brown <jobr3255@colorado.edu>
2019-12-11 17:36:23 -0700

    Updated database responses

--- api/database.js
+++ api/database.js
@@ -5,18 +5,79 @@ var connection = mysql.createConnection({
   password: 'Yummybrainz!2',
   database: 'cuhvmiwg_hvz'
 });
+// connection.connect()
+// connection.end()
+
+const STATUS = {
+  200: "OK",
+  201: "Created",
+  400: "Bad Request",
+  401: "Unauthorized",
+  403: "Forbidden",
+  404: "Not Found",
+  409: "Conflict"
+};
+
+function sqlError(res, status, err){
+  let errCode = "[" + err.code + "]";
+  let sqlMsg = " - " + err.sqlMessage;
+  res.status(status).json({
+    status: status,
+    error: {
+      message: errCode + sqlMsg,
+      code: STATUS[status],
+      sqlQuery: err.sql
+    }
+  });
+}
+
+function sendError(res, status, errorText){
+  res.status(status).json({
+    status: status,
+    error: {
+      message: errorText,
+      code: STATUS[status]
+    }
+  });
+}
+
+function sendResponse(res, status, data){
+  // res.status(status).json
+  res.status(status).json({
+    data: data,
+    status: status
+  });
+}
 
 var Database = function(){};
-Database.query = function (queryText, res) {
-  // connection.connect()
+Database.query = function (queryText, res, errorText = "No results found") {
   connection.query(queryText, function (err, data, fields) {
     if (err) {
-      res.send(err);
+      console.log(err);
+      sqlError(res, 400, err);
       return;
     }
-    res.send(data);
+    if(data.length == 0){
+      sendError(res, 404, errorText);
+      return;
+    }
+    sendResponse(res, 200, data);
   })
-  // connection.end()
+};
+
+Database.queryFetchSingle = function (queryText, res, errorText = "No results found") {
+  connection.query(queryText, function (err, data, fields) {
+    if (err) {
+      console.log(err);
+      sqlError(res, 400, err);
+      return;
+    }
+    if(data.length == 0){
+      sendError(res, 404, errorText);
+      return;
+    }
+    sendResponse(res, 200, data[0]);
+  })
 };
 
 module.exports = Database;
